bda5713ee9b88a65a75d9a6651eeaaf3410379ab
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-09-25 20:19:33 +0200

    J'ai finie le challenge 11 du condition

---
+++ conditions/Challenge 11 _ Jour de la Semaine.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main()
+{
+
+     srand(time(NULL));
+    int tim = rand()%7;
+    switch(tim){
+        case 0 :
+            printf("lundi");
+            break;
+        case 1:
+            printf("mardi");
+            break;
+        case 2 :
+            printf("mercredi");
+            break;
+        case 3 :
+            printf("jeudi");
+            break;
+        case 4 :
+            printf("vendredi");
+            break;
+        case 5 :
+            printf("samedi");
+            break;
+        case 6:
+            printf("dimanche");
+            break;
+
+
+     }
+
+
+
+
+
+    return 0;
+}

60b0f31c94590986465321254f0d78ce41483713
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-09-26 17:54:20 +0200

    J'ai termin la 6eme challenge

---
+++ Boucle/Challenge 6 _Affichage des N Premiers Nombres Pair.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+   int i,n,X;
+
+
+   printf(" Veuillez entrer un nombre entier:");
+   scanf("%d",&n);
+
+
+X=1;
+   for (i=1;i<=n;i++){
+      X=i*2;
+printf("%d premiers Nombres Pair%d",X);
+
+
+   }
+
+return 0;
+}
+
+
+

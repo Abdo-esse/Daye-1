3a86e8e8158039d79521913d687f7879e5b14901
Abdo-esse <esseabdo37@gmail.com>
2024-09-26 23:29:24 +0100

    J'ai termine le 10eme challenge du boucle

---
+++ Boucle/Challenge 10_ Calcul de la Somme des N Entier.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   int i,n,X;
+
+
+   printf(" Veuillez entrer un nombre entier:");
+   scanf("%d",&n);
+
+
+   X=0
+   for (i=1;i<=n;i++)
+
+      X=X+i;
+
+      printf(" %d",X);
+      return 0;
+
+
+}

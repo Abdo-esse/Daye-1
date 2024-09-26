a4bf833e3eed33a32180abee7bb8b918f3252fe6
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-09-26 17:49:29 +0200

    J'ai trmin la 4eme challenge du boucl

---
+++ Boucle/Challenge 4 _ Affichage des N Premiers Nombres Impairs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+   int i,n,X;
+
+
+   printf(" Veuillez entrer un nombre entier:");
+   scanf("%d",&n);
+
+
+
+   for (i=1;i<=n;i++){
+
+      X=i*2-1;
+      printf(" %d",X);
+
+
+   }
+
+return 0;
+}
+
+
+
